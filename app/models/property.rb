85301d01e7f657800e27214944f098c74cc49dfc
roryhellier <rpry.hellier@googlemail.com>
2021-02-23 23:06:47 +0000

    Begins adding model validations to the property model:
     - Adds validation for title for length and presence,
     - Adds validation for the user_type, for length and presence
     - Adds validation for the blurb, for length and presence
     - Addsa validation for the address, for length and presence

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,24 @@
 class Property < ApplicationRecord
+  validates :title, presence: true, length: { 
+    minimum: 1,
+    maximum: 140,
+    too_short: '%{count} is the minimum number of characters! Try to be more descriptive!',
+    too_long: '%{count}, is the maximum number of characters! Try shortening your title!'
+  }
+  validates :user_type, presence: true, inclusion: { in: %w(looking advertising) }
+  # Validate images - Check bookmarked article and sign up tpo cloudinary to host images
+  validates :blurb, presence: true, length: { 
+    minimum: 1,
+    maximum: 3000,
+    too_short: '%{count} is the minimum number of characters! Try to be more descriptive!',
+    too_long: '%{count}, is the maximum number of characters! Try shortening your description!'
+  }
+  validates :address, presence: true, length: {
+    minimum: 1,
+    maximum: 1000
+    too_short: '%{count} is the minimum number of characters! Please ensure you have entered your address fully.',
+    too_long: '%{count}, is the maximum number of characters! Try shortening your description!'
+  }
+
+
 end
