fae853498e7d78570eacef870471f4f9cea54654
roryhellier <rpry.hellier@googlemail.com>
2021-03-04 16:39:11 +0000

    Generates New API Controller For Properties And Adds CRUD Actions:
     - Generates new properties_controller in the api/v1 namespaced resource
     - Adds an index, create, show, update and destroy methods

---
+++ app/controllers/api/v1/properties_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::PropertiesController < ApplicationController
+  skip_before_action :authorized, only: %i[index show]
+
+  def index
+    @properties = Property.all
+    if @properties
+      render json: { properties: @properties }
+    else
+      render json: { status: 500, errors: ['Properties not found.'] }
+    end
+  end
+
+  def create
+    @property = Property.create(property_params)
+    if @property.valid?
+      render json: { property: PropertySerializer.new(@property) }, status: :created
+    else
+      render json: { errors: ['Failed to create property! Please try again.'] }, status: :not_acceptable
+    end
+  end
+
+  def show
+    @property = Property.find(property_params[:id])
+    if @property
+      render json: { property: @property }
+    else
+      render json: { status: 500, errors: ['Property not found!'] }
+    end
+  end
+
+  def update
+    @property = Property.find(property_params[:id])
+    if @property.update(property_params)
+      render json: { property: @property }
+    else
+      render json: { status: 500, errors: ['Something went wrong. Please try again later.'] }
+    end
+  end
+
+  def destroy
+    @property = Property.find(property_params[:id])
+    if @property.destroy
+      render json: { message: 'Property deleted.' }, status: :ok
+    else
+      render json: { status: 500, errors: ['Something went wrong. Please try again later.'] }
+    end
+  end
+end
