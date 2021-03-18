77a4891dd91f7164840514fc710fb5dfc9a57aa7
roryhellier <rpry.hellier@googlemail.com>
2021-03-18 16:26:12 +0000

    Adds New Routes For Updating and Destroying Users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       get '/auto-login', to: 'auth#auto_login'
       post '/signin', to: 'auth#create'
       get '/myaccount', to: 'users#profile'
+      post '/myaccount/settings/edit-profile', to: 'users#update',
+      post '/myaccount/settings/delete-profile', to: 'users#destroy',
       resources :properties, only: [:index]
       post '/new-property', to: 'properties#create'
       get '/property/:id', to: 'properties#show'
