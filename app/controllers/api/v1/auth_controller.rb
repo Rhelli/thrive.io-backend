91acafe37528f859ecae818a7d3224324b894b3d
Rhelli <rory.hellier@googlemail.com>
2021-04-08 14:17:22 +0100

    Resolves Error With The User Serializer Not Being Able To Read Current_User:
     - Modifies the user_serializer sugggested_properties method to reference 'self.object' instead of current_user, as the self.object is always the current_user
     - Returns the auth_controller to its original state

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::AuthController < ApplicationController
   def create
     @user = User.authenticate(user_login_params[:email], user_login_params[:password])
     if @user
-      @current_user = @user
+      current_user = @user
       token = encode_token({ user_id: @user.id })
-      render json: { current_user: UserSerializer.new(current_user), jwt: token }, status: :accepted
+      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
     else
       render json: { error: 'Invalid email address or password. Please try again.' }, status: :unauthorized
     end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,6 +3,6 @@ class UserSerializer < ActiveModel::Serializer
              :min_budget, :max_budget, :areas_looking, :advertiser_type, :shortlisted_properties, :properties, :suggested_properties
 
   def suggested_properties
-    User.relevant_properties(current_user)
+    User.relevant_properties(self.object)
   end
 end
